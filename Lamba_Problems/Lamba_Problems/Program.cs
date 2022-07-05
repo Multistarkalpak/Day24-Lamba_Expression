aeb21f1a5b7be3db1e65fd7fb5bb92f5d8743a43
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-07-05 10:23:54 +0530

    Adding data of person with default

---
+++ Lamba_Problems/Lamba_Problems/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Lamba_Problems
+{
+
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            List<Person> listpersonincity = new List<Person>();
+            listpersonincity.Add(new Person("4745424", "John", "12 main streat Newyork YK", 15));
+            listpersonincity.Add(new Person("35444442", "Aamir", "14 main streat UK YK", 50));
+            listpersonincity.Add(new Person("23403444", "Shahrukh", "17 main streat Mumbai YK", 56));
+            listpersonincity.Add(new Person("76888786", "Salman", "19 main streat Delhi YK", 70));
+            listpersonincity.Add(new Person("27687786", "Amresh", "13 main streat Assam YK", 53));
+            listpersonincity.Add(new Person("27867667", "Johnny", "15 main streat Madras YK", 62));
+            listpersonincity.Add(new Person("27687786", "Hritik", "20 main streat chennai YK", 45));
+            listpersonincity.Add(new Person("27867667", "Amitabh", "22 main streat Kolkata YK", 72));
+
+
+        }
+    }
+}
