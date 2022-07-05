fe9ec682e3a16eb52bbebb5a7d4aa3d42bc8c6c1
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-07-05 10:31:09 +0530

    Retrive two Person below 60

--- Lamba_Problems/Lamba_Problems/Program.cs
+++ Lamba_Problems/Lamba_Problems/Program.cs
@@ -6,7 +6,7 @@ namespace Lamba_Problems
     internal class Program
     {
         public static void Main(String[] args)
-        {
+        {    //UC1
             List<Person> listpersonincity = new List<Person>();
             listpersonincity.Add(new Person("4745424", "John", "12 main streat Newyork YK", 15));
             listpersonincity.Add(new Person("35444442", "Aamir", "14 main streat UK YK", 50));
@@ -17,6 +17,13 @@ namespace Lamba_Problems
             listpersonincity.Add(new Person("27687786", "Hritik", "20 main streat chennai YK", 45));
             listpersonincity.Add(new Person("27867667", "Amitabh", "22 main streat Kolkata YK", 72));
 
+            //UC2
+            Console.WriteLine("\n______________________________________");
+            Console.WriteLine("Retriving top 2 aged persons from the list who are below 60 years");
+            foreach (Person person in listpersonincity.FindAll(e => (e.Age <= 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name:" + person.Name + "\t\tAge:" + person.Age);
+            }
 
         }
     }
